cdb79da2003e42f03535cf251767104f8eefd4fe
orugu <94263067+orugu@users.noreply.github.com>
2025-10-25 16:59:35 +0900

    Update 1.01

--- EasyLogFire.py
+++ EasyLogFire.py
@@ -12,22 +12,23 @@ class Delivery(BaseModel):
         timestamp: datetime
         dimensions: tuple[int, int]
 
-
 m=Delivery(timestamp=datetime.now(),
-           dimensions=['10', '20'])
+           dimensions=(10, 20))
 timestatus= "["+str(m.timestamp.year)+"-"+str(m.timestamp.month)+"-"+str(m.timestamp.day)+" "+str(m.timestamp.hour)+":"+str(m.timestamp.minute)+":"+str(m.timestamp.second)+"] "
 
 
 
 def initialize_Easylogger(serv_name="default"):
     lf.configure(service_name = serv_name)
+
     lf.instrument_pydantic()
+
 
 
 def send_logs(Delivery=m, ctx="No message", extra_content="", ):
     lf.info(timestatus+ctx,
             extra={"timestamp": repr(Delivery.timestamp),
-                   "context": extra_content}
+                   "context": extra_content},
             )   
     
 
@@ -42,4 +43,4 @@ def initialize_Pydantic(serv_name= ""):
     print(m.dimensions)
 
     Delivery(timestamp='2020-01-02T03:04:05Z',
-             dimensions=['10', '10'],)
+             dimensions=(10, 10))
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import EasyLogFire as el
 
 def main():
-    el.initialize_Pydantic(serv_name="Easylogger")
+    el.initialize_Pydantic(serv_name="EasyLogFire")
     while(1):
-        print("로그 내용을 입력해주세요")
+        print("Please Type Log Context")
         context = input()
         el.send_logs(ctx=context)
 
